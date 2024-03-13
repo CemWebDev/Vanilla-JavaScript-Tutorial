a435ae1b08a07b96d12dab3a162511884a23d0fc
CemWebDev <137628926+CemWebDev@users.noreply.github.com>
2024-03-13 18:21:17 +0300

    Update app.js

--- JS-3/app.js
+++ JS-3/app.js
@@ -23,4 +23,48 @@ console.log(firstNumber); // error
 /* const name;
 name = "John"; */
 
+/* Javascript Data Types */
+
+//Numbers in Javascript
+
+const widht = 30;
+const height = 40;
+
+console.log(typeof(width)); // number
+consloe.log(typeof(height)); // number
+
+
+// Strings in Javascript
+
+const favoriteColor = "Yellow";
+const carBrand = "Mercedes";
+
+console.log(typeof(favoriteColor)); // string
+console.log(typeof(carBrand)); // string
+
+// Booleans in Javascript
+
+const isTrue = true;
+const isFalse = false;
+
+console.log(typeof(isTrue)); // boolean
+console.log(typeof(isFalse)); // boolean
+
+// Object in Javascript
+
+const student = {firstName: "John", lastName: "Doe", id: 2024};
+
+console.log(typeof(student)); // object
+
+
+/! Javascript types are dynamic. This means that the same variable can be used to hold different data types./
+
+let x; // x is undefined now.
+x = 12; // Now, x is a number.
+console.log(x); // 12
+console.log(typeof(x)); // number
+x = "John"; // Now, x is a string
+console.log(x); // John
+console.log(typeof(x)); // string
+
 
