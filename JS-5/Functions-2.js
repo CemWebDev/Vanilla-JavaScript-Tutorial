cae891a085294ab96bea8419581c5015ed4d0930
CemWebDev <137628926+CemWebDev@users.noreply.github.com>
2024-03-14 18:01:45 +0300

    Update and rename Functions-2 to Functions-2.js

---
+++ JS-5/Functions-2.js
@@ -0,0 +1,17 @@
+
+/* Javascript Arrow Functions */
+
+
+let greeting = () => {
+  console.log("Hello, world!");
+}
+greeting();// Hello, world!
+
+
+// Arrow functions with parameters
+
+let welcome = (firstName) => {
+  console.log("Welcome " + firstName + "!");
+}
+
+welcome("John"); // Welcome John!
