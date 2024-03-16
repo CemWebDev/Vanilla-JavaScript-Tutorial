a37d686de724153c49179a73872c55fd18ab7586
CemWebDev <137628926+CemWebDev@users.noreply.github.com>
2024-03-17 00:33:08 +0300

    Update Examples.js

--- JS-5/Examples.js
+++ JS-5/Examples.js
@@ -53,3 +53,29 @@ const fibonacci = (n) => {
 console.log(fibonacci(3)); // 2
 console.log(fibonacci(7)); // 13
 console.log(fibonacci(14)); // 377
+
+
+
+
+// Function to find the minimum and maximum values in an array
+const findMinMax = (arr) => {
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  // Return an object containing the min and max values
+  return { min, max };
+};
+
+const numbers = [3, 7, 2, 9, 1, 6, 4, 8, 5];
+
+console.log(findMinMax(numbers)); // {min: 1, max: 9} so, output is an object
+
+console.log(findMinMax(numbers).min); // 1
+console.log(findMinMax(numbers).max); // 9
