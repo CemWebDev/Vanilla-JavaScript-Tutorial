7f5b079fbea5c5d84b1a153672bdc6974659c9c0
CemWebDev <137628926+CemWebDev@users.noreply.github.com>
2024-03-17 00:23:56 +0300

    Update Examples.js

--- JS-5/Examples.js
+++ JS-5/Examples.js
@@ -38,3 +38,18 @@ calculate("-");
 calculate("*");
 calculate("/");
 
+
+
+// Recursive Fibonacci Function 
+
+const fibonacci = (n) => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+};
+
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(7)); // 13
+console.log(fibonacci(14)); // 377
