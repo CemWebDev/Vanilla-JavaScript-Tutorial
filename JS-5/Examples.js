05099667f99772ab6728a542557122738efd36a5
CemWebDev <137628926+CemWebDev@users.noreply.github.com>
2024-03-16 17:47:29 +0300

    Update Examples.js

--- JS-5/Examples.js
+++ JS-5/Examples.js
@@ -1,1 +1,34 @@
+// Simple Calculator Application
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+const calculate = (operation) => {
+  let result;
+  switch (operation) {
+    case "+":
+      result = add(1, 2);
+      break;
+    case "-":
+      result = subtract(1, 2);
+
+      break;
+    case "*":
+      result = multiply(1, 2);
+      break;
+    case "/":
+      result = divide(1, 2);
+      break;
+    default:
+      console.log("Invalid operation");
+      break;
+  }
+  console.log(result);
+};
+
+calculate("+");
+calculate("-");
+calculate("*");
+calculate("/");
 
