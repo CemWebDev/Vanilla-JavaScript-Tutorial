b3979c2e1e5d7e8fecd02945d200d9ded96c130c
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-03-17 03:47:07 +0300

    updated functions

--- JS-5/Examples.js
+++ JS-5/Examples.js
@@ -1,16 +1,23 @@
-// Simple Calculator Application
-const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
-const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
-const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
-const divide = (firstNumber, secondNumber) => {
-  if (secondNumber === 0) {
-    console.log("Invalid value");
-  } else {
-    return firstNumber / secondNumber;
-  }
-};
+/* Simple Calculator Application */
 
-const calculate = (operation) => {
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+function divide(firstNumber, secondNumber) {
+  if (secondNumber != 0) {
+    return firstNumber / secondNumber;
+  } else {
+    console.log("Invalid value");
+  }
+}
+
+function calculator(operation) {
   let result;
   switch (operation) {
     case "+":
@@ -18,7 +25,6 @@ const calculate = (operation) => {
       break;
     case "-":
       result = subtract(1, 2);
-
       break;
     case "*":
       result = multiply(1, 2);
@@ -27,38 +33,33 @@ const calculate = (operation) => {
       result = divide(1, 2);
       break;
     default:
-      console.log("Invalid operation");
+      console.log("Unknown operation");
       break;
   }
   console.log(result);
-};
+}
 
-calculate("+");
-calculate("-");
-calculate("*");
-calculate("/");
+calculator("+");
+calculator("-");
+calculator("*");
+calculator("/");
 
+// Recursive Fibonacci Function
 
-
-// Recursive Fibonacci Function 
-
-const fibonacci = (n) => {
+function fibonacci(n) {
   if (n <= 1) {
     return n;
   } else {
     return fibonacci(n - 1) + fibonacci(n - 2);
   }
-};
+}
 
-console.log(fibonacci(3)); // 2
 console.log(fibonacci(7)); // 13
 console.log(fibonacci(14)); // 377
 
-
-
-
 // Function to find the minimum and maximum values in an array
-const findMinMax = (arr) => {
+
+function findMinMax(arr) {
   let min = arr[0];
   let max = arr[0];
   for (let i = 0; i < arr.length; i++) {
@@ -71,7 +72,7 @@ const findMinMax = (arr) => {
   }
   // Return an object containing the min and max values
   return { min, max };
-};
+}
 
 const numbers = [3, 7, 2, 9, 1, 6, 4, 8, 5];
 
