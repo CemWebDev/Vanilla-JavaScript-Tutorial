7b0b2959d7ef93bec2087d7ab518e55054bfce7a
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-03-20 13:47:50 +0300

    update tutorial

--- JS-OOP/Encapsulation/app.js
+++ JS-OOP/Encapsulation/app.js
@@ -25,17 +25,22 @@ class Product {
     this.#name = name;
     this.#price = price;
   }
+  // Method the get name of the product
   getName() {
     return this.#name;
   }
+  // Method the get price of the product
   getPrice() {
     return this.#price;
   }
+  // Method the set price of the product
   setPrice(price) {
     this.#price = price;
   }
 }
 
 const product = new Product("Laptop", 2000);
+// Log the price of the product with the date "20.03.2024"
 console.log("20.03.2024 " + product.getPrice());
+// Log the price of the product with the date "20.04.2024"
 console.log("20.04.2024 " + product.getName());
