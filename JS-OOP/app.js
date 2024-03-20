e66294b0e4425643e3db0b40c669f737da3333a7
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-03-20 13:07:47 +0300

    updated JS-OOP

--- JS-OOP/app.js
+++ JS-OOP/app.js
@@ -1,3 +1,19 @@
+/* Objects */
+
+const car = {
+  color: "red",
+  age: 12,
+  introduction: function () {
+    console.log("The car is " + car.color);
+  }
+}
+console.log(car.color); // red
+console.log(car.age); // 12
+car.introduction(); // The car is red
+
+
+
+// Constructor method
 class Animal {
   constructor(species) {
     this.species = species;
