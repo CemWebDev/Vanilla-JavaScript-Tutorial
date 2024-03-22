666d03c1cfe27f5e3cef8ee28e0c459a11349d96
cemCankaya <137628926+cemCankaya@users.noreply.github.com>
2024-03-22 17:55:03 +0300

    update tutorial

--- 5-strings/strings.js
+++ 5-strings/strings.js
@@ -13,64 +13,11 @@ console.log(student2); // Forest Eads
 let newText = `javascript is awesome.`;
 console.log(newText); // Javascript is awesome.
 
-let age = 23;
+/* usage of template strings */
+let fullName = "John Smith";
+console.log(`Welcome ${fullName}!`); // Welcome John Smith!
 
+let age = 23;
 let info = `He is ${age} years old`;
 console.log(info); // He is 23 years old.
 
-//! Javascript String methods
-
-//! -Length-
-// To find the length of a string, use the length property
-let text = "JohnDoe";
-let lengthOfTheText = text.length;
-console.log(lengthOfTheText); // 7
-
-//! -charAt()-
-
-let greeting = "hello, world!";
-console.log(greeting.charAt(2)); // l
-console.log(greeting.charAt(8)); // o
-console.log(greeting.charAt(5)); // ,
-
-//! toLowerCase() and toUpperCase()
-
-let text1 = "hello, world!";
-let upperCaseText = text1.toUpperCase();
-console.log(upperCaseText); // hello, world!
-console.log(text1.toUpperCase()); // HELLO, WORLD!
-
-let text2 = "HELLO, WORLD";
-let lowerCaseText = text2.toLowerCase();
-console.log(lowerCaseText); // HELLO, WORLD!
-console.log(text2.toLowerCase()); // hello, world!
-
-//! -replace()-
-
-let text3 = "John Doe is 23 years old";
-console.log(text3); // John Doe is 23 years old
-let newText3 = text3.replace("23", "24");
-console.log(newText3); // John Doe is 24 years old
-
-//! -trim()-
-let text4 = "      hello, world!           ";
-//! The trim() method removes whitespace from both ends of a string
-console.log(text4.trim()); // hello, world!
-
-//! startsWith() and endsWith()
-
-let text5 = "Hello world!";
-console.log(text5.startsWith("Hello")); // true;
-console.log(text5.startsWith("World!")); // false
-
-console.log(text5.endsWith("world!")); // true;
-console.log(text5.endsWith("Hello!")); // false
-
-//! -concat()-
-
-/* To concatenate two or more strings, you can use the concat() method. */
-
-let text6 = "Hello";
-let text7 = "world!";
-let text8 = text6.concat(" ", text7);
-console.log(text8); // Hello world!
